2730d6c7a197cffa360a75ae24ad840483a43f64
+ <+>
2022-01-28 10:02:26 -0500

    commit tree 406491bdd0c727e972551beb8aa2de1c4dada80e

---
+++ src/components/ParentComponent.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react"
+import DisplayData from "./DisplayData";
+import Form from "./Form"
+import DisplayData from "./DisplayData";
+
+function ParentComponent() {
+
+    const [firstName, setFirstName] = useState("John");
+    const [lastName, setLastName] = useState("Henry");
+    const [newsletter, setNewsletter] = useState("false")
+
+    function handleNewsLetterChange(event) {
+        setNewsletter(event.target.checked)
+    }
+
+    function handleFirstNameChange(event) {
+        setFirstName(event.target.value)
+    }
+
+    function handleLastNameChange(event) {
+        setLastName(event.target.value)
+    }
+
+    return (
+        <Form firstName={firstName}
+            lastName={lastName}
+            newsletter={newsletter}
+            handleFirstNameChange={handleFirstNameChange}
+            handleLastNameChange={handleLastNameChange}
+            handleNewsLetterChange={handleNewsLetterChange}
+        />
+
+        <DisplayData lastName={lastName} firstName={firstName} />
+        )
+
+}
+
+            export default ParentComponent
